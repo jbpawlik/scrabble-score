e7951dcef23a9703a24651a234c0864730a7de39
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-07-13 11:45:03 -0700

    add test three; test passing
    Co-authored-by: Kylewilliamsonv1 <Kylewilliamsonv1@users.noreply.github.com>

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -6,8 +6,14 @@ describe('#scrabble') do
     word = Word.new('a')
     expect(word.scrabble).to(eq(1))
   end
-  it("returns a scrabble score for the word") do
+  it("returns a scrabble score for a word containing 1-point letters") do
     word = Word.new('lanes')
     expect(word.scrabble).to(eq(5))
   end
+  it("returns a scrabble score for a word containing 1- and 2-point letters") do
+    word = Word.new('danes')
+    expect(word.scrabble).to(eq(6))
+  end
+  it("")
+
 end
